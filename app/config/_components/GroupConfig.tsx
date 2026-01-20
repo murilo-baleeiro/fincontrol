ea17ccdfd881d7abdd877085a81aaf838c460041
devmuca <murilo.baleeiro@hotmail.com>
2026-01-20 00:29:13 -0300

    FIX: fixed visual glitch in categories items ul

--- app/config/_components/GroupConfig.tsx
+++ app/config/_components/GroupConfig.tsx
@@ -1,16 +1,21 @@
 "use client";
 
 import { ChevronDown, ChevronRight } from "lucide-react";
-import { ReactNode, useRef, useState } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 
-interface GroupConfigProps {
+interface GroupDownProps {
   title: string;
   children: ReactNode;
 }
 
-export default function GroupConfig({ title, children }: GroupConfigProps) {
+export default function GroupDown({ title, children }: GroupDownProps) {
   const [open, setOpen] = useState(false);
   const contentRef = useRef<HTMLDivElement>(null);
+  const [height, setHeight] = useState("0px");
+
+  useEffect(() => {
+    if (open && contentRef.current) setHeight(`${contentRef.current.scrollHeight}px`);
+  }, [children, open]); // 👈 recalcula quando o conteúdo muda
 
   return (
     <div className="border border-gray-200 rounded p-2">
@@ -21,9 +26,7 @@ export default function GroupConfig({ title, children }: GroupConfigProps) {
 
       <div
         ref={contentRef}
-        style={{
-          maxHeight: open ? `${contentRef.current?.scrollHeight}px` : "0px",
-        }}
+        style={{ maxHeight: open ? height : "0px" }}
         className="overflow-hidden transition-all duration-200 ease-out"
       >
         <div className="mt-2 px-1">{children}</div>
